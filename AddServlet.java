a739a232534054237f1d73dcaa13ccdd1ec72870
Banotbot <apapomsV2@gmail.com>
2025-09-19 08:46:46 +0800

    [BUG FIX] I already fix the bug

--- AddServlet.java
+++ AddServlet.java
@@ -30,6 +30,7 @@ public class AddServlet extends HttpServlet
 
 		    int addResult = i + j;
 			 int minusResult = i - j;
+			 int divide = i / j;
 			 
 			  
 			/*
@@ -41,6 +42,7 @@ public class AddServlet extends HttpServlet
 		    System.out.println("Num1 " + i);
 		    System.out.println("Num2 " + j);
 			 System.out.println("Minus Result => " + minusResult);
+			 System.out.println("Divide Result => " + divide);
 		    
 			//res.sendRedirect("sq?addResult=" + addResult); // SESSION MANAGEMENT where you can transfer data from another to another  
 			
